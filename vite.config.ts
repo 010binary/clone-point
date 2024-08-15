4823f69d758cb5e5c455fd9960e25478c3570d18
Pelumi Isola <C_isolapel@gcn.ab-inbev.com>
2024-08-15 17:16:45 +0100

    fix: husky removed and migrated to pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    viteTsconfigPaths(),
-    {
-      name: 'suppress-ts-errors',
-      transform(src, id) {
-        if (id.endsWith('.ts') || id.endsWith('.tsx')) {
-          return {
-            code: '/* @ts-nocheck */\n' + src,
-            map: null,
-          };
-        }
-        return null;
-      },
-    }
-  ],
+  plugins: [react()],
 })

098c28245723d5cba6c5a1fb741938f604505c6d
Pelumi Isola <C_isolapel@gcn.ab-inbev.com>
2024-08-25 17:32:30 +0100

    feat: customers data added

--- src/pages/crm/Dashboard.tsx
+++ src/pages/crm/Dashboard.tsx
@@ -1,6 +1,7 @@
 import DashboardLayout from "./layout";
-import BranchIcon from "../../assets/icons/branch.png";
-import TellerIcon from "../../assets/icons/tellers.png";
+import Customer from "../../assets/icons/customer-icon.png";
+import OfficeIcon from "../../assets/icons/office-icon.png";
+import { Link } from "react-router-dom";
 
 const CRMDashboard = () => {
   return (
@@ -10,25 +11,23 @@ const CRMDashboard = () => {
           className="w-full bg-white flex justify-between py-5 mt-5"
           style={{ boxShadow: "0px 4px 30px 0px #1A88E14A inset" }}
         >
-          <h6 className="font-bold text-black px-6">Today</h6>
+          <h6 className="font-bold text-black px-6"></h6>
         </div>
 
         <div className="flex  flex-wrap flex-row items-start gap-4 py-10">
           <div className="bg-gray-200 md:w-fit w-full border rounded-lg border-gray-300 flex gap-3 md:items-start items-center justify-center px-8 py-4">
-            <img src={BranchIcon} alt="" />
+            <img src={Customer} alt="" />
             <div>
-              <p className="text-center">Total Branch</p>
-              <h6 className="text-center font-bold">57</h6>
+              <p className="text-center">Customer</p>
             </div>
           </div>
 
-          <div className="bg-gray-200 md:w-fit w-full border rounded-lg border-gray-300 flex gap-3 md:items-start items-center justify-center px-8 py-4">
-            <img src={TellerIcon} alt="" />
+          <Link to="/admin/crm/office-data" className="bg-gray-200 md:w-fit w-full border rounded-lg border-gray-300 flex gap-3 md:items-start items-center justify-center px-8 py-4">
+            <img src={OfficeIcon} alt="" />
             <div>
-              <p className="text-center">All tellers</p>
-              <h6 className="text-center font-bold">25</h6>
+              <p className="text-center">Office (Employees)</p>
             </div>
-          </div>
+          </Link>
         </div>
       </div>
     </DashboardLayout>

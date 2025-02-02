62a5c232d034eb0571d7f621e2ba2367bbc91d87
Okechukwu Daniel <okechukwudaniel12@gmail.com>
2025-02-02 01:09:32 +0100

    push

---
+++ src/app/communication/email/_components/data.ts
@@ -0,0 +1,51 @@
+export interface smsType {
+  date: string;
+  name: string;
+  phoneNumber: string;
+  messageContent: string;
+  messageID: string;
+  status: string;
+}
+
+export const smsData: smsType[] = [
+  {
+    date: "2012001233",
+    phoneNumber: "0908938383",
+    name: "James John",
+    messageContent: "Seasons Greetings",
+    messageID:'23456',
+    status: "successful",
+  },
+  {
+    date: "2012001233",
+    phoneNumber: "0908938383",
+    name: "James John",
+    messageContent: "Seasons Greetings",
+    messageID:'23456',
+    status: "successful",
+  },
+  {
+    date: "2012001233",
+    phoneNumber: "0908938383",
+    name: "James John",
+    messageContent: "Seasons Greetings",
+    messageID:'23456',
+    status: "successful",
+  },
+  {
+    date: "2012001233",
+    phoneNumber: "0908938383",
+    name: "James John",
+    messageContent: "Seasons Greetings",
+    messageID:'23456',
+    status: "successful",
+  },
+  {
+    date: "2012001233",
+    phoneNumber: "0908938383",
+    name: "James John",
+    messageContent: "Seasons Greetings",
+    messageID:'23456',
+    status: "successful",
+  }
+];
